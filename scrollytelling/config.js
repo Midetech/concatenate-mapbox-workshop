c1e4efc9085b8ae9724243218557793ffb919c22
Midetech <faginorish@gmail.com>
2019-11-19 12:30:17 +0100

    Initial Commit

---
+++ scrollytelling/config.js
@@ -0,0 +1,63 @@
+var config = {
+    style: 'mapbox://styles/mideveloper/ck1j5ab531eq81dnz7qzvshix', //TODO
+    accessToken: 'pk.eyJ1IjoibWlkZXZlbG9wZXIiLCJhIjoiY2sxajQ1OXVrMTJlYzNtcDY5NGIxcHFnbiJ9.GZd4MQT1B1I-GXLebwGZyw', //TODO
+    title: 'Using Mapbox For the First Time', //TODO
+    subtitle: 'A Workshop at Concatenate', //TODO
+    byline: 'Mideveloper', //TODO
+    footer: 'Created at Concatenate Lagos 2019',
+    showMarkers: false,
+    theme: 'light',
+    alignment: 'left',
+    chapters: [
+        //TODO replace this data with data from the first point in your dataset
+        {
+          "location": {
+            "center": [
+              3.3595511425791926,
+              6.507029496381506
+            ],
+            "zoom": 10,
+            "pitch": 0,
+            "bearing": 0
+          },
+          "title": "Surulere",
+          "description": `
+          The place where Bode Thomas is the  road that lead.
+          <br />
+          <img src="https://res.cloudinary.com/mideveloper/image/upload/v1569851474/Photo-right_be7v3q.png" />
+          `
+        },
+        
+        {
+          "location": {
+            "center": [
+              3.359248297929639,
+              6.4382011783976765
+            ],
+            "zoom": 10,
+            "pitch": 0,
+            "bearing": 0
+          },
+          "title": "Apapa. Lagos",
+          "description": "Where I live "
+        },
+
+        {
+          "location": {
+            "center": [
+              3.3784338940440364,
+              6.499524997141847
+            ],
+            "zoom": 10,
+            "pitch": 0,
+            "bearing": 0
+          },
+          "title": "Yaba",
+          "description": "This is where my tech hub is located. CC-HUB, Lagos"
+        }
+        
+        
+        
+        //-paste more chapters here - one for each point in your dataset!
+    ]
+};
